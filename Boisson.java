9168bdf29644e681334ddc069cdff93242c1494d
Leroux <enzo.leroux@student.junia.com>
2023-11-30 11:26:00 +0100

    Stock etc..

--- Boisson.java
+++ Boisson.java
@@ -1,7 +1,22 @@
 import java.util.List;
 
-
 class Boisson {
     private String name;
     private int quantity;
+    private float prix;
 }
+    public void increaseQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    public void decreaseQuantity(int amount) {
+        this.quantity -= amount;
+    }
+
+    public String getNom() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
