d05677d1e654bb17b99961dc42099a56dc2b08e2
Paul POUSSET <paul.pousset@student.junia.com>
2023-11-30 11:30:22 +0100

    change polo

--- Barman.java
+++ Barman.java
@@ -1,8 +1,27 @@
 import java.util.List;
 
+public class Barman extends Employee {
 
-class Barman extends Employee {
-    public void prepareBoissons() {
-        // Implémenter la logique
+    // Constructeur
+    public Barman(String nom, String prenom, int age) {
+        super();
+    }
+
+    // Méthode pour préparer les boissons d'une commande
+    public void prepareBoissons(Order order) {
+        List<Boisson> boissonsApreparer = order.getBoissons();
+
+        if (boissonsApreparer.isEmpty()) {
+            System.out.println("Aucune boisson à préparer pour cette commande.");
+            return;
+        }
+
+        System.out.println("Préparation des boissons pour la table " + order.getTableNumber() + "...");
+        for (Boisson boisson : boissonsApreparer) {
+            // Logique spécifique pour la préparation de chaque boisson
+            System.out.println("Préparation de " + boisson.getNom());
+        }
+
+        System.out.println("Les boissons pour la table " + order.getTableNumber() + " sont prêtes.");
     }
 }
--- Order.java
+++ Order.java
@@ -1,5 +1,5 @@
+import java.util.ArrayList;
 import java.util.List;
-
 
 class Order {
     private List<Plat> plats;
@@ -7,7 +7,71 @@ class Order {
     private int tableNumber;
     private boolean pret;
 
-    public void addItem() {
-        // Implémenter la logique
+    // Constructeur
+    public Order(int tableNumber) {
+        this.plats = new ArrayList<>();
+        this.boissons = new ArrayList<>();
+        this.tableNumber = tableNumber;
+        this.pret = false;
+    }
+
+    // Ajouter un plat
+    public void addPlat(Plat plat) {
+        this.plats.add(plat);
+    }
+
+    // Ajouter une boisson
+    public void addBoisson(Boisson boisson) {
+        this.boissons.add(boisson);
+    }
+
+    // Getters et Setters
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public boolean isPret() {
+        return pret;
+    }
+
+    public void setPret(boolean pret) {
+        this.pret = pret;
+    }
+
+    // Calculer le total de la commande
+    public double getTotal() {
+        double total = 0;
+        for (Plat plat : plats) {
+            total += plat.getPrix();
+        }
+        for (Boisson boisson : boissons) {
+            total += boisson.getPrix();
+        }
+        return total;
+    }
+
+    // Afficher la commande
+    public void afficherCommande() {
+        System.out.println("Commande pour la table: " + tableNumber);
+        for (Plat plat : plats) {
+            System.out.println(plat);
+        }
+        for (Boisson boisson : boissons) {
+            System.out.println(boisson);
+        }
+        System.out.println("Total: " + getTotal() + " €");
+    }
+
+    public List<Boisson> getBoissons() {
+        return boissons;
+    }
+    // Marquer la commande comme prête
+    public void marquerCommePret() {
+        setPret(true);
     }
 }
+
